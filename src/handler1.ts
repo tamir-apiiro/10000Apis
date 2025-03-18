05d7336bbb7cf22ca1af64f72b147353e18f62d6
Tamir Levi <tamir@apiiro.com>
2025-03-18 18:40:26 +0200

    .

---
+++ src/handler1.ts
@@ -0,0 +1,3 @@
+
+export function handler1(req: any, res: any) {
+}
---
+++ src/server1.ts
@@ -0,0 +1,7 @@
+
+const express = require('express')
+import {handler1} from "./handler1";
+const app = express()
+app.get('/1', handler1)
+app.listen(3000, () => {})
+        
