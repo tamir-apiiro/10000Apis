bafac0dfabe26d3c1b2d79adcf2f1ae46079ed9c
Tamir Levi <tamir@apiiro.com>
2025-04-01 09:54:18 +0300

    import from non trivial path

--- src/server0.ts
+++ src/server0.ts
@@ -1,6 +1,6 @@
+import {handler0} from "@handlers/handler0";
 
 const express = require('express')
-import {handler0} from "./handler0";
 const app = express()
 app.get('/0', handler0)
 app.listen(3000, () => {})
